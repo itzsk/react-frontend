3dbc53a2d44b6e4c470613cfc26a2c9dbcf71c51
itzsk <103205418+itzsk@users.noreply.github.com>
2023-07-16 12:46:42 +0530

    uploded

---
+++ src/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./component/Header";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main className="pt-16 bg-slate-300 min-h-[calc(100vh)]">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+export default App;
